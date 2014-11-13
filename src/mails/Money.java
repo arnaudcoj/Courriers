65dad44d8c7bc791af19f4e9a27076801894c2dd
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-13 15:55:13 +0100

    text & money

---
+++ src/mails/Money.java
@@ -0,0 +1,33 @@
+/**
+ * TP Courriers
+ * @author Pierre Graux
+ * @author Arnaud Cojez
+ */
+package mails;
+
+/**
+ * Class defining the Money
+ */
+public class Money implements Content {
+
+	//Fields
+	protected int amount;
+
+	/**
+	 * Constructor for the Money class
+	 * @param amount
+	 */
+	public Money(int amount) throws IllegalArgumentException {
+		if(amount < 0)
+			throw new IllegalArgumentException("amount must be >= 0");
+		this.amount = amount;
+	}
+	
+	/**
+	 * returns the amount
+	 * @return the amount
+	 */
+	public int getAmount() {
+		return amount;
+	}
+}
---
+++ src/mails/Text.java
@@ -0,0 +1,35 @@
+/**
+ * TP Courriers
+ * @author Pierre Graux
+ * @author Arnaud Cojez
+ */
+package mails;
+
+/**
+ * Class defining the Text
+ */
+public class Text implements Content {
+
+	//Fields
+	protected String text;
+
+	//Methods
+	
+	/**
+	 * Constructor for the Text class
+	 * @param text
+	 */
+	public Text(String text) {
+		super();
+		this.text = text;
+	}
+	
+	/**
+	 * returns the text
+	 * @return the text
+	 */
+	public String getText() {
+		return text;
+	}
+
+}
