781984b0dfce2f3aad3e9be1496c52a446a49886
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-13 15:32:42 +0100

    Création inhabitant

---
+++ src/mails/Inhabitant.java
@@ -0,0 +1,21 @@
+/**
+ * TP Courriers
+ * @author Pierre Graux
+ * @author Arnaud Cojez
+ */
+package mails;
+
+/**
+ * Class defining the Inhabitant
+ */
+public class Inhabitant {
+
+	/**
+	 * @param letter
+	 */
+	public void receiveLetter(Letter<?> letter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
