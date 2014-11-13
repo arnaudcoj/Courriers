9f04f160399fcec4d8aaa4e581708f9411eed4d4
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-13 15:33:40 +0100

    Création Letter

--- src/mails/Letter.java
+++ src/mails/Letter.java
@@ -9,5 +9,13 @@ package mails;
  * Abstract Class defining the Letters
  */
 public abstract class Letter<T> {
+
+	/**
+	 * @return
+	 */
+	public Inhabitant getReceiver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 //TODO
 }
