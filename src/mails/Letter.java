4366fe3b1c2913c21a90d64914a575c0bec023c0
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-13 15:41:52 +0100

    final

--- src/mails/Letter.java
+++ src/mails/Letter.java
@@ -8,14 +8,60 @@ package mails;
 /**
  * Abstract Class defining the Letters
  */
-public abstract class Letter<T> {
+public abstract class Letter<T extends Content> {
 
+	//Attributes
+	protected Inhabitant sender;
+	protected Inhabitant receiver;
+	protected T content;
+	
+	//Methods
+	
 	/**
-	 * @return
+	 * Constructor for the Letter class
+	 * @param sender
+	 * @param receiver
+	 * @param content
+	 */
+	public Letter(Inhabitant sender, Inhabitant receiver, T content) {
+		super();
+		this.sender = sender;
+		this.receiver = receiver;
+		this.content = content;
+	}
+	
+	/**
+	 * Returns the sender of the letter
+	 * @return the sender
+	 */
+	public Inhabitant getSender() {
+		return sender;
+	}
+	
+	/**
+	 * Returns the receiver of the letter
+	 * @return the receiver
 	 */
 	public Inhabitant getReceiver() {
-		// TODO Auto-generated method stub
-		return null;
+		return receiver;
 	}
-//TODO
+	/**
+	 * Returns the content of the letter
+	 * @return the content
+	 */
+	public T getContent() {
+		return content;
+	}
+	
+	/**
+	 * Action executed when the letter is opened
+	 */
+	public abstract void action();
+	
+	/**
+	 * Returns the cost to send the letter
+	 * @return the cost to send the letter
+	 */
+	public abstract int getCost();
+
 }
