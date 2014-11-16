7b3a6ef2ea81dcb3a62f0a528dbfe44e92d6b913
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-16 15:16:08 +0100

    creation

---
+++ src/mails/test/AllTests.java
@@ -0,0 +1,19 @@
+/**
+ * TP Courriers
+ * @author Pierre Graux
+ * @author Arnaud Cojez
+ */
+package mails.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Class defining the AllTests
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ BankAccountTest.class, CityTest.class, InhabitantTest.class })
+public class AllTests {
+
+}
