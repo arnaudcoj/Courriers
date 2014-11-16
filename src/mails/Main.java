e532cea418684dbec0236cde99e64283c67649f7
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-16 15:17:37 +0100

    creation

---
+++ src/mails/Main.java
@@ -0,0 +1,21 @@
+/**
+ * TP Courriers
+ * @author Pierre Graux
+ * @author Arnaud Cojez
+ */
+package mails;
+
+/**
+ * Class defining the Main
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
