de3ef2ed59c4cb4d3617ecbfdff037cb8bce377d
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-16 15:03:16 +0100

    creation

---
+++ src/mails/test/InhabitantTest.java
@@ -0,0 +1,57 @@
+/**
+ * TP Courriers
+ * @author Pierre Graux
+ * @author Arnaud Cojez
+ */
+package mails.test;
+
+import static org.junit.Assert.assertEquals;
+import mails.BankAccount;
+import mails.City;
+import mails.Inhabitant;
+import mails.content.Money;
+import mails.letter.PromissoryNote;
+
+import org.junit.Test;
+
+/**
+ * Class defining the Inhabitant Tests
+ */
+public class InhabitantTest {
+
+	/**
+	 * Test method for {@link mails.Inhabitant#getCity()}.
+	 */
+	@Test
+	public void testGetCity() {
+		City c = new City("Testville");
+		Inhabitant h = new Inhabitant(c, new BankAccount(12));
+		assertEquals(c, h.getCity());
+	}
+
+	/**
+	 * Test method for {@link mails.Inhabitant#getBankAccount()}.
+	 */
+	@Test
+	public void testGetBankAccount() {
+		BankAccount b = new BankAccount(12);
+		Inhabitant h = new Inhabitant(new City("testVille"), b);
+		assertEquals(b, h.getBankAccount());
+	}
+
+	/**
+	 * Test method for {@link mails.Inhabitant#receiveLetter(mails.Letter)}.
+	 */
+	@Test
+	public void testReceiveLetter() {
+		City c = new City("Testville");
+		Inhabitant h1 = new Inhabitant(c, new BankAccount(12));
+		Inhabitant h2 = new Inhabitant(c, new BankAccount(12));
+		PromissoryNote p = new PromissoryNote(h1, h2, new Money(12));
+		c.sendLetter(p);
+		c.distributeLetters();
+		assertEquals(-1, h1.getBankAccount().getBalance());
+		assertEquals(23, h2.getBankAccount().getBalance());
+	}
+
+}
