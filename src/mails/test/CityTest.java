905d83aadd4da5a4bb14406c867279f706067044
patlepanda <patlepanda@Panda-Tower.lan>
2014-11-17 23:48:48 +0100

    Some test

--- src/mails/test/CityTest.java
+++ src/mails/test/CityTest.java
@@ -6,11 +6,15 @@
 package mails.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import mails.BankAccount;
 import mails.City;
 import mails.Inhabitant;
 import mails.content.Money;
+import mails.content.Text;
 import mails.letter.PromissoryNote;
+import mails.letter.SimpleLetter;
 
 import org.junit.Test;
 
@@ -32,6 +36,25 @@ public class CityTest {
 		c.distributeLetters();
 		assertEquals(-1, h1.getBankAccount().getBalance());
 		assertEquals(23, h2.getBankAccount().getBalance());
+	}
+	
+	
+	@Test
+	public void testIsEmpty() {
+		City c = new City("Testville");
+		Inhabitant h1 = new Inhabitant(c, "Test-01", new BankAccount(12));
+		Inhabitant h2 = new Inhabitant(c, "Test-02", new BankAccount(12));
+		SimpleLetter p = new SimpleLetter(h1, h2, new Text("bla bla"));
+		assertTrue(c.isEmpty());
+		c.sendLetter(p);
+		assertFalse(c.isEmpty());
+		c.distributeLetters();
+		assertTrue(c.isEmpty());
+	}
+	
+	@Test
+	public void testToString() {
+		assertEquals(new City("Testville").toString(),"Testville city");
 	}
 
 }
